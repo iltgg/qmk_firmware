111f819d6962a4af4c19a0a6d0b819e8477a91df
ben <benlow999@gmail.com>
2024-05-06 19:32:55 -0400

    gaming layer

--- keyboards/chocofi/keymaps/iltgg/keymap.c
+++ keyboards/chocofi/keymaps/iltgg/keymap.c
@@ -84,19 +84,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,KC_NO,KC_NO,          KC_MS_BTN2,KC_MS_BTN1,KC_MS_BTN3
     ),
     // Gaming Layer - Modified QWERTY
-    // [9] = LAYOUT_split_3x5_3(
-    //     ,,,,, ,,,,,
-    //     ,,,,, ,,,,,
-    //     ,,,,, ,,,,,
-    //     ,,, ,,
-    // ),
+    [9] = LAYOUT_split_3x5_3(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                                KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,                                KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                                KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                                   MO(10),  KC_LALT, KC_SPC,              TG(9), KC_NO,  KC_NO
+    ),
     // Gaming Layer Extend
-    // [10] = LAYOUT_split_3x5_3(
-    //     ,,,,, ,,,,,
-    //     ,,,,, ,,,,,
-    //     ,,,,, ,,,,,
-    //     ,,, ,,
-    // ),
+    [10] = LAYOUT_split_3x5_3(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_T,                               KC_NO,     KC_NO,     KC_NO,   KC_NO,      KC_NO,
+        KC_5,    KC_6,    KC_7,    KC_8,    KC_G,                               KC_LEFT,   KC_DOWN,   KC_UP,   KC_RIGHT,   KC_NO,
+        KC_9,    KC_0,    KC_C,    KC_V,    KC_B,                               KC_NO,     KC_NO,     KC_NO,   KC_NO,      KC_NO,
+                                   KC_NO,   KC_NO, KC_NO,                KC_NO, KC_NO,     KC_NO
+    ),
 };
 
 #ifdef OLED_ENABLE
