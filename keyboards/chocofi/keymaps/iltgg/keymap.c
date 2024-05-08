4756e3fe2e44e2e0b58292e832dbeb6b47b4e403
ben <benlow999@gmail.com>
2024-05-08 01:56:04 -0400

    tap dance layers

--- keyboards/chocofi/keymaps/iltgg/keymap.c
+++ keyboards/chocofi/keymaps/iltgg/keymap.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include "action.h"
 #include "keymap_us.h"
+#include "process_tap_dance.h"
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
@@ -11,6 +12,10 @@ enum custom_keycodes {
     KC_MS_FU,
     KC_MS_FD,
     KC_MS_FR
+};
+
+enum {
+    TD_EXT_MOUSE
 };
 
 enum layer_names {
@@ -42,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_X,    KC_P,    KC_D,    KC_M,    KC_Q,                               KC_QUOT, KC_Y,    KC_O,    KC_U,    KC_SCLN,
         KC_S,    KC_N,    KC_T,    KC_H,    KC_V,                               KC_G,    KC_C,    KC_A,    KC_E,    KC_I,
         KC_F,    KC_B,    KC_K,    KC_L,    KC_J,                               KC_Z,    KC_W,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   MO(3),   KC_R, MO(4),               KC_BSPC, KC_SPC,  MO(2)
+                                   TD(TD_EXT_MOUSE),   KC_R, MO(4),               KC_BSPC, KC_SPC,  MO(2)
     ),
     // QWERTY Layer
     [1] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   MO(3),   KC_NO, MO(4),              KC_BSPC, KC_SPC,  KC_LSFT
+                                   TD(TD_EXT_MOUSE),   KC_NO, MO(4),              KC_BSPC, KC_SPC,  KC_LSFT
     ),
     // Shift Layer
     [2] = LAYOUT_split_3x5_3(
@@ -63,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(8),         MO(7),         KC_NO,         KC_NO,         KC_INS,                                  KC_HOME, KC_PGDN,  KC_PGUP,  KC_END,    KC_CAPS,
         OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_RALT),                           KC_LEFT, KC_DOWN,  KC_UP,    KC_RIGHT,  KC_DEL,
         C(KC_Z),       C(KC_X),       C(KC_C),       C(KC_V),       KC_LGUI,                                 KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_PSCR,
-                                                     KC_NO,         KC_NO,         KC_NO,           KC_ESC,  KC_ENT,  KC_TAB
+                                                     TD(TD_EXT_MOUSE),         KC_NO,         KC_NO,           KC_ESC,  KC_ENT,  KC_TAB
     ),
     // Symbol Layer
     [4] = LAYOUT_split_3x5_3(
@@ -98,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_ACL0, KC_ACL1, KC_ACL2, KC_NO,                          KC_MS_FL,      KC_MS_FD,      KC_MS_FU,    KC_MS_FR,       KC_NO,
         KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO,                          KC_MS_L,       KC_MS_D,       KC_MS_U,     KC_MS_R,        KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_WH_RIGHT, KC_NO,
-                                   KC_NO,   KC_NO, KC_NO,       KC_MS_BTN2, KC_MS_BTN1,    KC_MS_BTN3
+                                   TD(TD_EXT_MOUSE),   KC_NO, KC_NO,       KC_MS_BTN2, KC_MS_BTN1,    KC_MS_BTN3
     ),
     // Gaming Layer - Modified QWERTY
     [9] = LAYOUT_split_3x5_3(
@@ -114,6 +119,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_9,    KC_0,    KC_C,    KC_V,    KC_B,                               KC_NO,     KC_NO,     KC_NO,   KC_NO,      KC_NO,
                                    KC_NO,   KC_NO, KC_NO,                KC_NO, KC_NO,     KC_NO
     ),
+};
+
+void dance_ext_mouse_each(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            layer_on(3);
+            break;
+        case 2:
+            layer_on(8);
+    }
+}
+
+void dance_ext_mouse_reset(tap_dance_state_t *state, void *user_data) {
+    layer_off(3);
+    layer_off(8);
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    // Single tap moves to extend, double moves to mouse with no delay
+    [TD_EXT_MOUSE] = ACTION_TAP_DANCE_FN_ADVANCED(dance_ext_mouse_each, NULL, dance_ext_mouse_reset)
 };
 
 void set_fast_mouse(direction key) {
