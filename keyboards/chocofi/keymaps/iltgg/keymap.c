6791ad1d930bb53389749e86f7de866cee5429b3
ben <benlow999@gmail.com>
2024-05-09 01:50:38 -0400

    num tap dance numpad and minor edits

--- keyboards/chocofi/keymaps/iltgg/keymap.c
+++ keyboards/chocofi/keymaps/iltgg/keymap.c
@@ -16,7 +16,8 @@ enum custom_keycodes {
 
 enum {
     TD_EXT_MOUSE,
-    TD_SYM_FUNC
+    TD_SYM_FUNC,
+    TD_NUM_PAD
 };
 
 enum layer_names {
@@ -48,13 +49,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_X,    KC_P,    KC_D,    KC_M,    KC_Q,                                    KC_QUOT, KC_Y,    KC_O,    KC_U,    KC_SCLN,
         KC_S,    KC_N,    KC_T,    KC_H,    KC_V,                                    KC_G,    KC_C,    KC_A,    KC_E,    KC_I,
         KC_F,    KC_B,    KC_K,    KC_L,    KC_J,                                    KC_Z,    KC_W,    KC_COMM, KC_DOT,  KC_SLSH,
-                       TD(TD_EXT_MOUSE),    KC_R, MO(_NUMBER),      TD(TD_SYM_FUNC), KC_SPC,  MO(_SHIFT)
+                       TD(TD_EXT_MOUSE),    KC_R, TD(TD_NUM_PAD),      TD(TD_SYM_FUNC), KC_SPC,  MO(_SHIFT)
     ),
     [_QWERTY] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                       TD(TD_EXT_MOUSE),    KC_NO, MO(_NUMBER),     TD(TD_SYM_FUNC), KC_SPC,  MO(_QWERTY_SHIFT)
+                       TD(TD_EXT_MOUSE),    KC_NO, TD(TD_NUM_PAD),     TD(TD_SYM_FUNC), KC_SPC,  MO(_QWERTY_SHIFT)
     ),
     [_SHIFT] = LAYOUT_split_3x5_3(
         S(KC_X),    S(KC_P),    S(KC_D),    S(KC_M),    S(KC_Q),                        S(KC_QUOT), S(KC_Y),    S(KC_O),    S(KC_U),    S(KC_SCLN),
@@ -69,28 +70,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 C(KC_BSPC), KC_NO, KC_BSPC,              KC_NO, KC_NO,  KC_NO
     ),
     [_EXTEND] = LAYOUT_split_3x5_3(
-        KC_NO,         KC_NO,         KC_NO,         KC_NO,            KC_INS,                                  KC_HOME, KC_PGDN,  KC_PGUP,  KC_END,    KC_CAPS,
+        OSM(MOD_RALT), OSM(MOD_RSFT), OSM(MOD_RGUI), OSM(MOD_RCTL),    KC_LALT,                                 KC_HOME, KC_PGDN,  KC_PGUP,  KC_END,    KC_CAPS,
         OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL),    OSM(MOD_RALT),                           KC_LEFT, KC_DOWN,  KC_UP,    KC_RIGHT,  KC_DEL,
-        C(KC_Z),       C(KC_X),       C(KC_C),       C(KC_V),          KC_LGUI,                                 KC_NO,   KC_BSPC,  KC_NO,    KC_NO,     KC_PSCR,
+        C(KC_Z),       C(KC_X),       C(KC_C),       C(KC_V),          KC_LGUI,                                 KC_NO,   KC_BSPC,  KC_NO,    KC_INS,    KC_PSCR,
                                                      TD(TD_EXT_MOUSE), KC_NO,         KC_NO,           KC_ESC,  KC_ENT,  KC_TAB
     ),
     [_SYMBOL] = LAYOUT_split_3x5_3(
         KC_EXLM,       KC_AT,         KC_HASH,       KC_DLR,        KC_PERC,                              KC_EQL,  KC_GRV,  KC_DQUO, KC_QUOT, KC_MINUS,
         OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), KC_CIRC,                              KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_UNDS,
         KC_NO,         KC_NO,         KC_BSLS,       KC_PIPE,       KC_AMPR,                              KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_PLUS,
-                                                     KC_NO,         KC_NO,   KC_NO,    TD(TD_SYM_FUNC), KC_COLN, KC_SCLN
+                                                     KC_SCLN,       KC_COLN,   KC_NO,    TD(TD_SYM_FUNC), KC_NO,   KC_NO
     ),
     [_NUMBER] = LAYOUT_split_3x5_3(
-        KC_NO,         MO(_NUMPAD),   KC_COMM,       KC_DOT,        KC_NUM,                            KC_EQL,  KC_7, KC_8, KC_9, KC_MINUS,
-        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_RALT),                     KC_ASTR, KC_4, KC_5, KC_6, KC_PLUS,
-        MO(5),         KC_SPC,        KC_TAB,        KC_BSPC,       KC_ENT,                            KC_0,    KC_1, KC_2, KC_3, KC_SLSH,
-                                                     KC_NO,         KC_NO,         KC_NO,       KC_NO, KC_NO,   KC_NO
+        KC_NO,         KC_NO,         KC_COMM,       KC_DOT,        KC_NUM,                                    KC_EQL,  KC_7, KC_8, KC_9, KC_MINUS,
+        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_RALT),                             KC_ASTR, KC_4, KC_5, KC_6, KC_PLUS,
+        KC_NO,         KC_NO,        KC_TAB,         KC_ENT,        KC_NO,                                     KC_0,    KC_1, KC_2, KC_3, KC_SLSH,
+                                                     KC_NO,         KC_NO,         TD(TD_NUM_PAD),    KC_BSPC, KC_SPC,  KC_NO
     ),
     [_NUMPAD] = LAYOUT_split_3x5_3(
-        KC_NO,         KC_NO,         KC_PCMM,       KC_PDOT,       KC_NUM,                            KC_PEQL, KC_P7, KC_P8, KC_P9, KC_PMNS,
-        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_RALT),                     KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS,
-        KC_NO,         KC_SPC,        KC_TAB,        KC_BSPC,       KC_PENT,                           KC_P0,   KC_P1, KC_P2, KC_P3, KC_PSLS,
-                                                     KC_NO,         KC_NO,         KC_NO,       KC_NO, KC_NO,   KC_NO
+        KC_NO,         KC_NO,         KC_PCMM,       KC_PDOT,       KC_NUM,                                    KC_PEQL, KC_P7, KC_P8, KC_P9, KC_PMNS,
+        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_RALT),                             KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS,
+        KC_NO,         KC_NO,         KC_TAB,        KC_PENT,       KC_NO,                                     KC_P0,   KC_P1, KC_P2, KC_P3, KC_PSLS,
+                                                     KC_NO,         KC_NO,         TD(TD_NUM_PAD),    KC_BSPC, KC_SPC,  KC_NO
     ),
     [_FUNCTION] = LAYOUT_split_3x5_3(
         KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                             KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_SCRL,
@@ -148,10 +149,27 @@ void dance_sym_func_reset(tap_dance_state_t *state, void *user_data) {
     layer_off(_FUNCTION);
 }
 
+
+void dance_num_pad_each(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            layer_on(_NUMBER);
+            break;
+        case 2:
+            layer_on(_NUMPAD);
+    }
+}
+
+void dance_num_pad_reset(tap_dance_state_t *state, void *user_data) {
+    layer_off(_NUMBER);
+    layer_off(_NUMPAD);
+}
+
 tap_dance_action_t tap_dance_actions[] = {
     // Single tap moves to extend, double moves to mouse with no delay
     [TD_EXT_MOUSE] = ACTION_TAP_DANCE_FN_ADVANCED(dance_ext_mouse_each, NULL, dance_ext_mouse_reset),
-    [TD_SYM_FUNC] = ACTION_TAP_DANCE_FN_ADVANCED(dance_sym_func_each, NULL, dance_sym_func_reset)
+    [TD_SYM_FUNC] = ACTION_TAP_DANCE_FN_ADVANCED(dance_sym_func_each, NULL, dance_sym_func_reset),
+    [TD_NUM_PAD] = ACTION_TAP_DANCE_FN_ADVANCED(dance_num_pad_each, NULL, dance_num_pad_reset)
 };
 
 void set_fast_mouse(direction key) {
