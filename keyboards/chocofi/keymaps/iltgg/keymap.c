129b3e0bb6f046a43caf9f2953d00652ea590688
ben <benlow999@gmail.com>
2024-05-08 21:05:35 -0400

    move number layer to left hand

--- keyboards/chocofi/keymaps/iltgg/keymap.c
+++ keyboards/chocofi/keymaps/iltgg/keymap.c
@@ -45,40 +45,40 @@ bool fast_mode[] = {false, false, false, false};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SNTH] = LAYOUT_split_3x5_3(
-        KC_X,    KC_P,    KC_D,    KC_M,    KC_Q,                               KC_QUOT, KC_Y,    KC_O,    KC_U,    KC_SCLN,
-        KC_S,    KC_N,    KC_T,    KC_H,    KC_V,                               KC_G,    KC_C,    KC_A,    KC_E,    KC_I,
-        KC_F,    KC_B,    KC_K,    KC_L,    KC_J,                               KC_Z,    KC_W,    KC_COMM, KC_DOT,  KC_SLSH,
-                       TD(TD_EXT_MOUSE),    KC_R, TD(TD_SYM_FUNC),     KC_BSPC, KC_SPC,  MO(_SHIFT)
+        KC_X,    KC_P,    KC_D,    KC_M,    KC_Q,                                    KC_QUOT, KC_Y,    KC_O,    KC_U,    KC_SCLN,
+        KC_S,    KC_N,    KC_T,    KC_H,    KC_V,                                    KC_G,    KC_C,    KC_A,    KC_E,    KC_I,
+        KC_F,    KC_B,    KC_K,    KC_L,    KC_J,                                    KC_Z,    KC_W,    KC_COMM, KC_DOT,  KC_SLSH,
+                       TD(TD_EXT_MOUSE),    KC_R, MO(_NUMBER),      TD(TD_SYM_FUNC), KC_SPC,  MO(_SHIFT)
     ),
     [_QWERTY] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                       TD(TD_EXT_MOUSE),    KC_NO, TD(TD_SYM_FUNC),    KC_BSPC, KC_SPC,  MO(_QWERTY_SHIFT)
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                       TD(TD_EXT_MOUSE),    KC_NO, MO(_NUMBER),     TD(TD_SYM_FUNC), KC_SPC,  MO(_QWERTY_SHIFT)
     ),
     [_SHIFT] = LAYOUT_split_3x5_3(
         S(KC_X),    S(KC_P),    S(KC_D),    S(KC_M),    S(KC_Q),                        S(KC_QUOT), S(KC_Y),    S(KC_O),    S(KC_U),    S(KC_SCLN),
         S(KC_S),    S(KC_N),    S(KC_T),    S(KC_H),    S(KC_V),                        S(KC_G),    S(KC_C),    S(KC_A),    S(KC_E),    S(KC_I),
         S(KC_F),    S(KC_B),    S(KC_K),    S(KC_L),    S(KC_J),                        S(KC_Z),    S(KC_W),    S(KC_COMM), S(KC_DOT),  S(KC_SLSH),
-                                            KC_NO,      S(KC_R), MO(_NUMBER),    KC_NO, KC_NO,      KC_NO
+                                            C(KC_BSPC), S(KC_R), KC_BSPC,        KC_NO, KC_NO,      KC_NO
     ),
     [_QWERTY_SHIFT] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   KC_NO,   KC_NO, MO(_NUMBER),          KC_NO, KC_NO,  KC_NO
+                                C(KC_BSPC), KC_NO, KC_BSPC,              KC_NO, KC_NO,  KC_NO
     ),
     [_EXTEND] = LAYOUT_split_3x5_3(
         KC_NO,         KC_NO,         KC_NO,         KC_NO,            KC_INS,                                  KC_HOME, KC_PGDN,  KC_PGUP,  KC_END,    KC_CAPS,
         OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL),    OSM(MOD_RALT),                           KC_LEFT, KC_DOWN,  KC_UP,    KC_RIGHT,  KC_DEL,
-        C(KC_Z),       C(KC_X),       C(KC_C),       C(KC_V),          KC_LGUI,                                 KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_PSCR,
+        C(KC_Z),       C(KC_X),       C(KC_C),       C(KC_V),          KC_LGUI,                                 KC_NO,   KC_BSPC,  KC_NO,    KC_NO,     KC_PSCR,
                                                      TD(TD_EXT_MOUSE), KC_NO,         KC_NO,           KC_ESC,  KC_ENT,  KC_TAB
     ),
     [_SYMBOL] = LAYOUT_split_3x5_3(
         KC_EXLM,       KC_AT,         KC_HASH,       KC_DLR,        KC_PERC,                              KC_EQL,  KC_GRV,  KC_DQUO, KC_QUOT, KC_MINUS,
         OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), KC_CIRC,                              KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_UNDS,
         KC_NO,         KC_NO,         KC_BSLS,       KC_PIPE,       KC_AMPR,                              KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_PLUS,
-                                                     KC_NO,         KC_NO,   TD(TD_SYM_FUNC),    KC_SCLN, KC_COLN, MO(_NUMBER)
+                                                     KC_NO,         KC_NO,   KC_NO,    TD(TD_SYM_FUNC), KC_COLN, KC_SCLN
     ),
     [_NUMBER] = LAYOUT_split_3x5_3(
         KC_NO,         MO(_NUMPAD),   KC_COMM,       KC_DOT,        KC_NUM,                            KC_EQL,  KC_7, KC_8, KC_9, KC_MINUS,
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                             KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_SCRL,
         OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), KC_NO,                             KC_F5, KC_F6,  KC_F7,  KC_F8,  TG(_QWERTY),
         KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                             KC_F9, KC_F10, KC_F11, KC_F12, TG(_GAMING),
-                                                     KC_NO,         KC_NO, TD(TD_SYM_FUNC),     KC_NO, KC_NO, KC_NO
+                                                     KC_NO,         KC_NO, KC_NO,     TD(TD_SYM_FUNC), KC_NO, KC_NO
     ),
     [_MOUSE] = LAYOUT_split_3x5_3(
         KC_NO,   KC_ACL0, KC_ACL1, KC_ACL2,          KC_NO,                          KC_MS_FL,      KC_MS_FD,      KC_MS_FU,    KC_MS_FR,       KC_NO,
