b09dd1be2d76f78a11bf173be8901edf88b26db8
Benjamin Low <benlow999@gmail.com>
2025-03-20 23:07:41 -0400

    macro layer

--- keyboards/chocofi/keymaps/iltgg/keymap.c
+++ keyboards/chocofi/keymaps/iltgg/keymap.c
@@ -19,7 +19,7 @@ enum custom_keycodes {
 enum {
     TD_EXT_MOUSE,
     TD_SYM_FUNC,
-    TD_NUM_PAD,
+    TD_NUM_MACRO,
     TD_SHIFT_CTRL,
     TD_SHIFT_CTRL_Q,
     TD_CBOOT
@@ -36,6 +36,7 @@ enum layer_names {
     _SYMBOL,         // Symbols
     _NUMBER,         // Numbers
     _NUMPAD,         // Numbers with actual numpad keycodes (e.g. Blender)
+    _MACRO,          // Macros, and key combos
     _FUNCTION,       // Function keys and layer swaps
     _MOUSE,          // Mouse keys
     _GAMING,         // QWERTY right shifted with mods on column 1
@@ -56,13 +57,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_X,    KC_P,    KC_D,    KC_M,    KC_Q,                                       KC_MINUS, KC_Y,    KC_O,    KC_U,    KC_QUOT,
         KC_S,    KC_N,    KC_T,    KC_H,    KC_V,                                       KC_G,     KC_C,    KC_A,    KC_E,    KC_I,
         KC_F,    KC_B,    KC_K,    KC_L,    KC_J,                                       KC_Z,     KC_W,    KC_COMM, KC_DOT,  KC_SLSH,
-                       TD(TD_EXT_MOUSE),    KC_R, TD(TD_NUM_PAD),      TD(TD_SYM_FUNC), KC_SPC,   TD(TD_SHIFT_CTRL)
+                       TD(TD_EXT_MOUSE),    KC_R, TD(TD_NUM_MACRO),    TD(TD_SYM_FUNC), KC_SPC,   TD(TD_SHIFT_CTRL)
     ),
     [_QWERTY] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                       TD(TD_EXT_MOUSE),    KC_SPC, TD(TD_NUM_PAD),      TD(TD_SYM_FUNC), KC_SPC,  TD(TD_SHIFT_CTRL_Q)
+                       TD(TD_EXT_MOUSE),    KC_SPC, TD(TD_NUM_MACRO),    TD(TD_SYM_FUNC), KC_SPC,  TD(TD_SHIFT_CTRL_Q)
     ),
     [_SHIFT] = LAYOUT_split_3x5_3(
         S(KC_X),    S(KC_P),    S(KC_D),    S(KC_M),    S(KC_Q),                        S(KC_MINUS), S(KC_Y),    S(KC_O),    S(KC_U),    S(KC_QUOT),
@@ -102,17 +103,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUMBER] = LAYOUT_split_3x5_3(
         KC_LEFT,       KC_RIGHT,      KC_NO,         KC_COMM,       KC_NUM,                                    KC_EQL,  KC_7, KC_8, KC_9, KC_MINUS,
-        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_RALT),                             KC_ASTR, KC_4, KC_5, KC_6, KC_PLUS,
+        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), KC_NO,                                     KC_ASTR, KC_4, KC_5, KC_6, KC_PLUS,
         KC_DOWN,       KC_UP,         KC_NO,         KC_NO,         KC_NO,                                     KC_SPC,  KC_1, KC_2, KC_3, KC_SLSH,
-                                                     KC_NO,         KC_NO,         TD(TD_NUM_PAD),    KC_BSPC, KC_0,    KC_DOT
+                                                     MO(_NUMPAD),   KC_NO,         TD(TD_NUM_MACRO),    KC_BSPC, KC_0,    KC_DOT
     ),
     [_NUMPAD] = LAYOUT_split_3x5_3(
         KC_LEFT,       KC_RIGHT,      KC_NO,         KC_PCMM,       KC_NUM,                                    KC_PEQL, KC_P7, KC_P8, KC_P9, KC_PMNS,
-        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), OSM(MOD_RALT),                             KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS,
+        OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), KC_NO,                                     KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS,
         KC_DOWN,       KC_UP,         KC_NO,         KC_NO,         KC_NO,                                     KC_SPC,  KC_P1, KC_P2, KC_P3, KC_PSLS,
-                                                     KC_NO,         KC_NO,         TD(TD_NUM_PAD),    KC_BSPC, KC_P0,   KC_PDOT
+                                                     KC_NO,         KC_NO,         TD(TD_NUM_MACRO),    KC_BSPC, KC_P0,   KC_PDOT
+    ),
+    [_MACRO] = LAYOUT_split_3x5_3( // TODO: fill in macros
+        KC_X,    KC_P,    KC_D,    KC_M,    KC_Q,                                       KC_MINUS, KC_Y,    KC_O,    KC_U,    KC_QUOT,
+        KC_S,    KC_N,    KC_T,    KC_H,    KC_V,                                       KC_G,     KC_C,    KC_A,    KC_E,    KC_I,
+        KC_F,    KC_B,    KC_K,    KC_L,    KC_J,                                       KC_Z,     KC_W,    KC_COMM, KC_DOT,  KC_SLSH,
+                          KC_NO,   KC_R,    TD(TD_NUM_MACRO),                    KC_NO, KC_SPC,   KC_NO
     ),
     [_FUNCTION] = LAYOUT_split_3x5_3(
+        //                                           MIC
         KC_MPRV,       KC_MPLY,       KC_MNXT,       C(KC_F8),      KC_NO,                             KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_SCRL,
         OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LCTL), KC_NO,                             KC_F5, KC_F6,  KC_F7,  KC_F8,  TG(_QWERTY),
         KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                             KC_F9, KC_F10, KC_F11, KC_F12, TG(_GAMING),
@@ -169,19 +177,19 @@ void dance_sym_func_reset(tap_dance_state_t *state, void *user_data) {
 }
 
 
-void dance_num_pad_each(tap_dance_state_t *state, void *user_data) {
+void dance_num_macro_each(tap_dance_state_t *state, void *user_data) {
     switch (state->count) {
         case 1:
             layer_on(_NUMBER);
             break;
         case 2:
-            layer_on(_NUMPAD);
+            layer_on(_MACRO);
     }
 }
 
-void dance_num_pad_reset(tap_dance_state_t *state, void *user_data) {
+void dance_num_macro_reset(tap_dance_state_t *state, void *user_data) {
     layer_off(_NUMBER);
-    layer_off(_NUMPAD);
+    layer_off(_MACRO);
 }
 
 void dance_shift_ctrl_each(tap_dance_state_t *state, void *user_data) {
@@ -224,7 +232,7 @@ tap_dance_action_t tap_dance_actions[] = {
     // Single tap moves to extend, double moves to mouse with no delay
     [TD_EXT_MOUSE] = ACTION_TAP_DANCE_FN_ADVANCED(dance_ext_mouse_each, NULL, dance_ext_mouse_reset),
     [TD_SYM_FUNC] = ACTION_TAP_DANCE_FN_ADVANCED(dance_sym_func_each, NULL, dance_sym_func_reset),
-    [TD_NUM_PAD] = ACTION_TAP_DANCE_FN_ADVANCED(dance_num_pad_each, NULL, dance_num_pad_reset),
+    [TD_NUM_MACRO] = ACTION_TAP_DANCE_FN_ADVANCED(dance_num_macro_each, NULL, dance_num_macro_reset),
     [TD_SHIFT_CTRL] = ACTION_TAP_DANCE_FN_ADVANCED(dance_shift_ctrl_each, NULL, dance_shift_ctrl_reset),
     [TD_SHIFT_CTRL_Q] = ACTION_TAP_DANCE_FN_ADVANCED(dance_shift_ctrl_each_Q, NULL, dance_shift_ctrl_reset_Q),
     [TD_CBOOT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cboot_finished, NULL)
@@ -322,6 +330,9 @@ bool render_status(void) {
             break;
         case _NUMPAD:
             oled_write_P(PSTR("NUMPAD\n"), false);
+            break;
+        case _MACRO:
+            oled_write_P(PSTR("MACRO\n"), false);
             break;
         case _FUNCTION:
             oled_write_P(PSTR("FUNCTION\n"), false);
